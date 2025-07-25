641380ae6d6e7b83476c49c5abd8b5a21b15744b
Ilya Mazaev <limp2006@ya.ru>
2025-07-26 01:35:23 +0300

    delete unuse function

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -18,35 +18,37 @@ static const char *TAG = "example";
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define LED_PIN CONFIG_LED_GPIO
+#define CONTACT_PIN_1 CONFIG_CONTACT_PIN_GPIO_1
+#define CONTACT_PIN_2 CONFIG_CONTACT_PIN_GPIO_2
 
 static uint8_t s_led_state = 0;
 
 static void blink_led(void)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
+    gpio_set_level(LED_PIN, s_led_state);
 }
 
-static void configure_led(void)
+static void configure_pin(void)
 {
     ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
+    gpio_reset_pin(LED_PIN);
     /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 }
 
 void app_main(void)
 {
 
     /* Configure the peripheral according to the LED type */
-    configure_led();
+    configure_pin();
 
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "OFF" : "ON");
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        vTaskDelay(CONFIG_CHECK_PERIOD / portTICK_PERIOD_MS);
     }
 }
